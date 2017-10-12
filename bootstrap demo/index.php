cef329ea78fa9c9135a0386c3f19b5ea04278fa1
wesleymeyerwm <wesleymeyerwm@hotmail.com>
2017-10-12 12:50:33 +0200

    Voeg bootstrap demo toe aan project

---
+++ bootstrap demo/index.php
@@ -0,0 +1,34 @@
+<?php include 'modules/menu.php'; ?>
+
+<!-- Container -->
+<div class="container"> 
+
+  <!-- Jumbotron row -->
+  <div class="row">
+    <div class="jumbotron">
+      <h1>Bootstrap Demo</h1>      
+      <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+      <a class="btn btn-primary" href="#" role="button">Button voorbeeld</a>
+    </div>
+  </div>
+  <!-- /Jumbotron row -->
+  <!-- Content row -->
+  <div class="row">
+    <div class="col-md-4">
+      <h2>Colom md-4</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lorem arcu, pretium eget dictum id, commodo sit amet neque. Maecenas pellentesque lacus nec accumsan vulputate.</p>
+    </div>
+    <div class="col-md-4">
+      <h2>Colom md-4</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lorem arcu, pretium eget dictum id, commodo sit amet neque. Maecenas pellentesque lacus nec accumsan vulputate.</p>
+    </div>
+    <div class="col-md-4">
+      <h2>Colom md-4</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lorem arcu, pretium eget dictum id, commodo sit amet neque. Maecenas pellentesque lacus nec accumsan vulputate.</p>
+    </div>
+  </div>
+  <!-- /content row -->
+</div>
+<!-- /container -->
+  
+<?php include 'modules/footer.php'; ?>
