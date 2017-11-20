0a968478e367a3e038644075b6b213d318b0753a
wesleymeyerwm <wesleymeyerwm@hotmail.com>
2017-11-20 11:58:30 +0100

    Voeg header/footer toe
    Push header/footer voor start homepage

--- Blog/index.php
+++ Blog/index.php
@@ -2,29 +2,40 @@
 
 <!-- Container -->
 <div class="container"> 
-
-  <!-- Jumbotron row -->
-  <div class="row">
-    <div class="jumbotron">
-      <h1>Bootstrap Demo</h1>      
-      <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
-      <a class="btn btn-primary" href="#" role="button">Button voorbeeld</a>
-    </div>
-  </div>
-  <!-- /Jumbotron row -->
   <!-- Content row -->
   <div class="row">
-    <div class="col-md-4">
-      <h2>Colom md-4</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lorem arcu, pretium eget dictum id, commodo sit amet neque. Maecenas pellentesque lacus nec accumsan vulputate.</p>
+    <div class="col-md-1">
+      <p>
+        Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
+      </p>
     </div>
-    <div class="col-md-4">
-      <h2>Colom md-4</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lorem arcu, pretium eget dictum id, commodo sit amet neque. Maecenas pellentesque lacus nec accumsan vulputate.</p>
+  </div>
+  <div class="row">
+    <div class="col-md-1">
+      <p>
+        Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
+      </p>
     </div>
-    <div class="col-md-4">
-      <h2>Colom md-4</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lorem arcu, pretium eget dictum id, commodo sit amet neque. Maecenas pellentesque lacus nec accumsan vulputate.</p>
+  </div>
+  <div class="row">
+    <div class="col-md-1">
+      <p>
+        Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-1">
+      <p>
+        Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-1">
+      <p>
+        Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
+      </p>
     </div>
   </div>
   <!-- /content row -->
