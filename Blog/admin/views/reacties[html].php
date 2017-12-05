f951f9cf4fc887160c08aed4492c9d66659e4785
Wesley Meijer <wesleymeyerwm@hotmail.com>
2017-12-05 12:57:59 +0100

    WIP push juiste html layout naar reacties.php

--- Blog/admin/views/reacties[html].php
+++ Blog/admin/views/reacties[html].php
@@ -5,59 +5,66 @@ include '../../admin/header.php';
 require '../classes/database.php';
 ?>
 
+<!-- Content website -->
 <div class="content-wrapper">
-<div class="container-fluid">
-  <!-- Breadcrumbs-->
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item">
-      <a href="../admin/dashboard.php">Dashboard</a>
-    </li>
-    <li class="breadcrumb-item active">Overzicht</li>
-  </ol>
-  <div class="row">
-    <div class="col-12">
-        <h1><i class="fa fa-pencil"></i>
-        <span class="">Overzicht</span><h1>
-    </div>
-  </div>
-</div>
- <div class="card mb-3">
-    <div class="card-header">
-      <a href="../admin/overzicht.php"> Alles</a> |
-      <a href="#"> Gepubliceerd</a> |
-      <a href="#"> Concepten</a> |
-      <a href="#"> Verwijderd</a> |
-      <a href="../admin/toevoegen.php"> Toevoegen</a>
-      <form class="form-inline my-2 my-lg-0 mr-lg-2 float-right">
-        <div class="input-group">
-          <input class="form-control" type="text" placeholder="Zoeken...">
-          <span class="input-group-btn">
-            <button class="btn btn-secondary" type="button">
-              <i class="fa fa-search"></i>
-            </button>
-          </span>
-        </div>
-      </form>
-    </div>
-    <div class="card-body">
-      <div class="table-responsive">
-        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Titel</th>
-              <th>Geschreven door</th>
-              <th>Publiceerdatum</th>
-              <th>Gepubliceerd</th>
-              <th></th>
-            </tr>
-          </thead>
-        </table>
+    <div class="container-fluid">
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="../admin/dashboard.php">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item active">Overzicht</li>
+        </ol>
+        <div class="row">
+            <div class="col-12">
+                <h1><i class="fa fa-pencil"></i>
+                    <span class="">Overzicht</span><h1>
+                        </div>
+                        </div>
+                        </div>
+                        <div class="card mb-3">
+                            <div class="card-header">
+                                <a href="../admin/overzicht.php"> Alles</a> |
+                                <a href="gepubliceerd.php"> Gepubliceerd</a> |
+                                <a href="concepten.php"> Concepten</a> |
+                                <a href="verwijderd.php"> Verwijderd</a> |
+                                <a href="../admin/toevoegen.php"> Toevoegen</a>
+                                <form class="form-inline my-2 my-lg-0 mr-lg-2 float-right">
+                                    <div class="input-group">
+                                        <input class="form-control" type="text" placeholder="Zoeken...">
+                                        <span class="input-group-btn">
+                                            <button class="btn btn-secondary" type="button">
+                                                <i class="fa fa-search"></i>
+                                            </button>
+                                        </span>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                        <thead>
+                                            <tr>
+                                                <th>Artikelnr</th>
+                                                <th>Titel</th>
+                                                <th>Geschreven door</th>
+                                                <th>Publiceerdatum</th>
+                                                <th>#</th>
+                                                <th>#</th>
+                                                <th>Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+<!-- Tabel -->
 
-      </div>
-    </div>
-    <div class="card-footer small text-muted">Laatst bijgewerkt "Time"</div>
-  </div>
-</div>
+<!-- Footer -->
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="card-footer small text-muted">Laatst bijgewerkt 11:59 PM</div>
+                            </div>
+                          </div>
+                        </div>
 
 <?php include '../footer.php'; ?>
