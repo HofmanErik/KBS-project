bd9f62e6312ec0387851af12432a5294c7ebc7ae
Wesley Meijer <wesleymeyerwm@hotmail.com>
2017-12-05 12:51:48 +0100

    WIP voeg html layout toe voor reactieoverzicht
    Html layout gekopieerd uit overzicht.php voor het testen van de layout. // Css bestanden worden niet aangeroepen

--- Blog/admin/classes/Database.php
+++ Blog/admin/classes/Database.php
@@ -28,24 +28,24 @@ class database{
 		$this->stmt = $this->dbh->prepare($query);
 	}
 
-	public function bind($param, $value, $type = null){
-		if(is_null($type)){
-			switch(true){
-				case is_int($value):
-					$type = PDO::PARAM_INT;
-					break;
-				case is_bool($value):
-					$type = PDO::PARAM_BOOL;
-					break;
-				case is_null($value):
-					$type = PDO::PARAM_NULL;
-					break;
-					default:
-					$type = PDO::PARAM_STR;
-			}
-		}
-		$this->stmt->bindValue($param, $value, $type);
-	}
+	// public function bind($param, $value, $type = null){
+	// 	if(is_null($type)){
+	// 		switch(true){
+	// 			case is_int($value):
+	// 				$type = PDO::PARAM_INT;
+	// 				break;
+	// 			case is_bool($value):
+	// 				$type = PDO::PARAM_BOOL;
+	// 				break;
+	// 			case is_null($value):
+	// 				$type = PDO::PARAM_NULL;
+	// 				break;
+	// 				default:
+	// 				$type = PDO::PARAM_STR;
+	// 		}
+	// 	}
+	// 	$this->stmt->bindValue($param, $value, $type);
+	// }
 
 	public function execute(){
 		return $this->stmt->execute();
--- Blog/admin/views/reacties[html].php
+++ Blog/admin/views/reacties[html].php
@@ -1,14 +1,65 @@
-<?php 
+<?php
+// Include bestanden
 include '../../admin/header.php';
 include '../header.php';
-require '../classes/database.php' 
+require '../classes/database.php';
 ?>
 
 <link href="'../../vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
-<!-- Databaseconnectie -->
 
-<div>
-	
+<div class="content-wrapper">
+<div class="container-fluid">
+  <!-- Breadcrumbs-->
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item">
+      <a href="../admin/dashboard.php">Dashboard</a>
+    </li>
+    <li class="breadcrumb-item active">Overzicht</li>
+  </ol>
+  <div class="row">
+    <div class="col-12">
+        <h1><i class="fa fa-pencil"></i>
+        <span class="">Overzicht</span><h1>
+    </div>
+  </div>
+</div>
+ <div class="card mb-3">
+    <div class="card-header">
+      <a href="../admin/overzicht.php"> Alles</a> |
+      <a href="#"> Gepubliceerd</a> |
+      <a href="#"> Concepten</a> |
+      <a href="#"> Verwijderd</a> |
+      <a href="../admin/toevoegen.php"> Toevoegen</a>
+      <form class="form-inline my-2 my-lg-0 mr-lg-2 float-right">
+        <div class="input-group">
+          <input class="form-control" type="text" placeholder="Zoeken...">
+          <span class="input-group-btn">
+            <button class="btn btn-secondary" type="button">
+              <i class="fa fa-search"></i>
+            </button>
+          </span>
+        </div>
+      </form>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Titel</th>
+              <th>Geschreven door</th>
+              <th>Publiceerdatum</th>
+              <th>Gepubliceerd</th>
+              <th></th>
+            </tr>
+          </thead>
+        </table>
+
+      </div>
+    </div>
+    <div class="card-footer small text-muted">Laatst bijgewerkt "Time"</div>
+  </div>
 </div>
 
-<?php include '../admin/footer.php'; ?>
+<?php include '../footer.php'; ?>
