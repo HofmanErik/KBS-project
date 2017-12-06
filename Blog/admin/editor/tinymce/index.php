bf94d299135de45ba6b3150ae02a33f1db5b8c48
Shanna99 <shannavangrevengoed@live.nl>
2017-12-06 14:31:26 +0100

    img

--- Blog/admin/editor/tinymce/index.php
+++ Blog/admin/editor/tinymce/index.php
@@ -11,7 +11,7 @@
     plugins: [
       'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
       'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
-      'save table contextmenu directionality emoticons template paste textcolor'
+      'save table contextmenu directionality emoticons template paste textcolor image'
     ],
     content_css: 'css/content.css',
     toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | print preview media fullpage | forecolor backcolor emoticons'
