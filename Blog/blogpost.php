493e42b3b5b80c7c233e5c0d2ff5c3a7d4f01763
Shanna99 <shannavangrevengoed@live.nl>
2017-12-06 13:47:43 +0100

    blogpost php code

--- Blog/blogpost.php
+++ Blog/blogpost.php
@@ -1,103 +1,80 @@
 <?php include 'blank-template.php'?>
 
-<div class="container">
-  <div class="row title">
-    <div class="col-md-8">
-      <div class="title-post">
-        <h1>  <?php
-        $servername = "localhost";
-        $username = "beheerder";
-        $password = "geheim";
-        $dbname = "db_vindbaarin";
-        $sql = "SELECT artikelnr, titel, tekst, datum, voornaam FROM artikel a JOIN medewerker m ON WHERE artikelnr = 66";
+ <?php
+
+if(!isset($_GET["artikelnr"])){
+  header("location: index.php");
+}else{
+  $artikelnr = $_GET["artikelnr"];
 
 
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-        } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
-
-        while ($row = $stmt->fetch()) {
-            print("<tr>");
-            print("<td><h1>" . $row["Titel"] . "</h1></td>");
-            print("</tr>");
-        }
-
-        ?></h1>
-          <p class="post-meta">Posted by
-            <a href="#">Vindbaar In</a>
-            <?php
-            $servername = "localhost";
-            $username = "beheerder";
-            $password = "geheim";
-            $dbname = "db_vindbaarin";
-            $sql = "SELECT datum FROM artikel WHERE artikelnr = 66";
+  $servername = "localhost";
+  $username = "beheerder";
+  $password = "geheim";
+  $dbname = "db_vindbaarin";
+  $sql = "SELECT artikelnr, titel, tekst, thumbnail, datum, voornaam FROM artikel a JOIN medewerker m ON a.auteur = m.mnr WHERE artikelnr = :artikel1";
 
 
-            try {
-                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $stmt = $conn->prepare($sql);
-                $stmt->execute();
-            } catch (PDOException $e) {
-                echo "Connection failed: " . $e->getMessage();
-            }
+  try {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $stmt = $conn->prepare($sql);
+      $stmt -> bindValue(':artikel1', $artikelnr, PDO::PARAM_INT);
+      $stmt->execute();
+  } catch (PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+  }
 
-            while ($row = $stmt->fetch()) {
-                print("<tr>");
-                print("<td>" . "on " . $row["datum"] . "</td>");
-                print("</tr>");
-            }
+  while ($row = $stmt->fetch()) {
+      $titel = $row['titel'];
+      $tekst = $row['tekst'];
+      $thumbnail = $row['thumbnail'];
+      $datum = $row['datum'];
+      $voornaam = $row['voornaam'];
 
-            ?></p>
-      </div>
-    </div>
-    <div class="col-md-4">
+  }
+}
 
-      <img src="download.png" alt="vierkantje"
-            class="img-responsive post-img">
-    </div>
 
-  </div>
-</div>
+        ?>
+
+
+        <div class="container">
+          <div class="row title">
+            <div class="col-md-8">
+              <div class="title-post">
+                <h1><?= $titel; ?> </h1>
+                  <p class="post-meta">Posted by
+                    <a href="#"><?= $voornaam; ?></a>
+                      <?= $datum; ?></p>
+              </div>
+            </div>
+            <div class="col-md-4">
+
+              <img src= <?= $thumbnail; ?>
+                    class="img-responsive post-img">
+            </div>
+
+          </div>
+        </div>
 
 
 
-          <div class="container">
-            <div class="row text">
-              <div class="col-md-10 col-md-offset-1">
-                <div class="post-text">
-        <p><?php
-        $servername = "localhost";
-        $username = "beheerder";
-        $password = "geheim";
-        $dbname = "db_vindbaarin";
-        $sql = "SELECT tekst FROM artikel WHERE artikelnr = 66";
+
+        <div class="container">
+          <div class="row text">
+            <div class="col-md-8 col-md-offset-1">
+              <div class="post-text">
+                <?= $tekst; ?>
+
+                </div>
+              </div>
+            </div>
+          </div>
 
 
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-        } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
 
-        while ($row = $stmt->fetch()) {
-            print("<tr>");
-            print("<td>" .  $row["tekst"] . "</td>");
-            print("</tr>");
-        }
 
-        ?></p>
-      </div>
-    </div>
-  </div>
-</div>
+
 
 <?php include 'modules/footer.php'; ?>
