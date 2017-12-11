4164666d6f3f1c36ac7fd33aa8327287c5c32dbd
Melistick <Melistick@users.noreply.github.com>
2017-12-11 13:51:46 +0100

    again

--- Blog/admin/verwerktoevoegen.php
+++ Blog/admin/verwerktoevoegen.php
@@ -20,36 +20,18 @@
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-<<<<<<< HEAD
-=======
-
->>>>>>> bdf4c228b4c7a59178cdaf0bf19c59a8e952f41f
   //Als er op de button Publiceren geklikt wordt dan:
   if(isset($_POST['Publiceren'])){
     $valid = true;
     $filevalid = false;
-<<<<<<< HEAD
     $file = $_FILES['thumbnail'];
-=======
-
-    $file = $_FILES['thumbnail'];
-
->>>>>>> bdf4c228b4c7a59178cdaf0bf19c59a8e952f41f
     $fileName = $_FILES['thumbnail']['name'];
     $fileTmp = $_FILES['thumbnail']['tmp_name'];
     $fileSize = $_FILES['thumbnail']['size'];
     $fileError = $_FILES['thumbnail']['error'];
-<<<<<<< HEAD
-=======
-
->>>>>>> bdf4c228b4c7a59178cdaf0bf19c59a8e952f41f
     $fileExt = explode('.', $fileName);
     $fileActualExt = strtolower(end($fileExt));
     $allow = array('jpg','jpeg','png');
-<<<<<<< HEAD
-=======
-
->>>>>>> bdf4c228b4c7a59178cdaf0bf19c59a8e952f41f
     if(in_array($fileActualExt, $allow)){
       //kijken of het filetype klopt
       if($fileError === 0){
@@ -67,10 +49,6 @@
     }else {
       header("location: toevoegen.php?filetype");
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> bdf4c228b4c7a59178cdaf0bf19c59a8e952f41f
     //Titel invoeren
     $titel = htmlentities(trim($_POST['titel'], ENT_QUOTES));
     //checken of titel is ingevuld
@@ -81,10 +59,6 @@
         </div>';
         $valid = false;
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> bdf4c228b4c7a59178cdaf0bf19c59a8e952f41f
     $tekst= htmlentities(trim($_POST['tinymce'], ENT_QUOTES));
     //checken of tekst is ingevuld
     if(empty($tekst)){
@@ -94,36 +68,19 @@
       </div>';
       $valid = false;
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> bdf4c228b4c7a59178cdaf0bf19c59a8e952f41f
     if($valid == true && $filevalid ==true){
       $stmt = $conn ->prepare("INSERT INTO artikel (artikelnr, titel, tekst, thumbnail, auteur, datum, afbeelding, status)
                              VALUES ('?', '$titel','$tekst','$fileNameNew', '1', NOW(), '?', 1 )");
       $stmt->execute();
-<<<<<<< HEAD
       //Hier worden de thumbnails opgeslagen
       $fileDestination = 'afbeeldingopslag/' . $fileNameNew;
       move_uploaded_file($fileTmp, $fileDestination);
-=======
-
-      //Hier worden de thumbnails opgeslagen
-      $fileDestination = 'afbeeldingopslag/' . $fileNameNew;
-      move_uploaded_file($fileTmp, $fileDestination);
-
-
->>>>>>> bdf4c228b4c7a59178cdaf0bf19c59a8e952f41f
       echo'
       <div class="container-fluid">
         Artikel is gepubliceerd!
       </div>';
     }
   }
-<<<<<<< HEAD
-=======
-
->>>>>>> bdf4c228b4c7a59178cdaf0bf19c59a8e952f41f
   $conn->close();
 ?>
 <?php include "../admin/footer.php"; ?>
