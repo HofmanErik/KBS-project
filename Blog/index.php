c1c3de2092358c3a885bd062ca2bc811a01be64e
Melistick <Melistick@users.noreply.github.com>
2017-12-11 13:15:07 +0100

    preview

--- Blog/index.php
+++ Blog/index.php
@@ -24,47 +24,24 @@
           $password = "geheim";
           $dbname = "db_vindbaarin";
           $sql = "SELECT * FROM artikel a JOIN medewerker m ON a.auteur = m.mnr WHERE status =1 ORDER BY datum DESC";
-                    try {
-                      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                      $stmt = $conn->prepare($sql);
-                      $stmt->execute();
-                    } catch (PDOException $e) {
-                      echo "Connection failed: " . $e->getMessage();
-                    }
 
-                    while ($row = $stmt->fetch()) {
-                      $thumbnail = $row["thumbnail"];
-                      if($row["thumbnail"] == ""){
-                        $thumbnail = "download.png";
-                    }
-                    $thumbsource = "admin/afbeeldingopslag/" . $thumbnail;
 
-                    print('
-                    <div class="post-preview">
-                      <div class="row">
-                        <div class="col-md-3">
-                          <img src="'.$thumbsource.'" alt="'.$thumbsource.'"class="img-responsive blogimg img-circle">
-                        </div>
-                        <div class="col-md-9">
-                          <a href="blogpost.php?artikelnr='.$row["artikelnr"].'">
-                            <h2 class="post-title">
-                                '.$row["titel"].'
-                            </h2>
-                            <h5 class="post-subtitle">
-                              Hier komt een preview van de tekst alleen zijn we er nog niet helemaal over uit hoe
-                            </h5>
-                          </a>
-                          <p class="post-meta">Posted by
-                            <a href="#">'.$row["voornaam"].'</a>
-                               '.$row["datum"].'</p>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-lg-4 col-md-10 mx-auto"></div>
-                    <hr>
-                    <hr>');
-                  }
+          try {
+              $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+              $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              $stmt = $conn->prepare($sql);
+              $stmt->execute();
+          } catch (PDOException $e) {
+              echo "Connection failed: " . $e->getMessage();
+          }
+
+          while ($row = $stmt->fetch()) {
+            $thumbnail = $row["thumbnail"];
+              if($row["thumbnail"] == ""){
+                $thumbnail = "download.png";
+                }
+                $thumbsource = "admin/afbeeldingopslag/" . $thumbnail;
+
                 // preview 
                 list($a,$b,$c,$rest) = explode(".",$row["tekst"]);
                 $tekst = ($a.$b.$c);
@@ -107,6 +84,6 @@
         </div>
       </div>
     </div>
-    <hr>
 
+    <hr>
 <?php include 'footer.php';?>
