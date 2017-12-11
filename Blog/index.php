98d8d406ea27897d5ab082a88d08420581e3fd75
Shanna99 <shannavangrevengoed@live.nl>
2017-12-11 10:37:34 +0100

    Merch

--- Blog/index.php
+++ Blog/index.php
@@ -43,7 +43,6 @@
                     }
                     $thumbsource = "admin/afbeeldingopslag/" . $thumbnail;
 
-<<<<<<< HEAD
                     print('
                     <div class="post-preview">
                       <div class="row">
@@ -69,41 +68,7 @@
                     <hr>
                     <hr>');
                   }
-=======
-          while ($row = $stmt->fetch()) {
-            $thumbnail = $row["thumbnail"];
-              if($row["thumbnail"] == ""){
-                $thumbnail = "download.png";
-                }
-                $thumbsource = "admin/afbeeldingopslag/" . $thumbnail;
 
-        print('
-          <div class="post-preview">
-           <div class="row">
-             <div class="col-md-3">
-              <img src="'.$thumbsource.'" alt="'.$thumbsource.'"class="img-responsive blogimg2 bloground">
-            </div>
-            <div class="col-md-9">
-              <a href="blogpost.php?artikelnr='.$row["artikelnr"].'">
-                <h2 class="post-title">
-                '.$row["titel"].'
-                </h2>
-                <h4 class="post-subtitle">
-                Hier komt een preview van de tekst alleen zijn we er nog niet helemaal over uit hoe
-                </h4>
-              </a>
-              <p class="post-meta">Posted by
-              <a href="#">'.$row["voornaam"].'</a>
-              '.$row["datum"].'</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-10 mx-auto">
-          </div>
-          <hr>
-          <hr>');
-        }
->>>>>>> c784e9ed6f9bae066219fe4730399a0719820ff9
           ?>
           <!-- Pager -->
           <!--<div class="clearfix">
@@ -113,5 +78,5 @@
       </div>
     </div>
     <hr>
-    
+
 <?php include 'footer.php';?>
