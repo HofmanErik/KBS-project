25ad28224f7b917dc2a22b71af1de4ad037bda64
wesleymeyerwm <wesleymeyerwm@hotmail.com>
2017-12-12 14:47:36 +0100

    Maak testdatabaseconnectie aan vanwege errors

--- Blog/admin/classes/queries.php
+++ Blog/admin/classes/queries.php
@@ -1,5 +1,18 @@
 <?php
 
+// Database connectie
+$servername = "localhost";
+$username = "beheerder";
+$password = "geheim";
+$dbname = "db_vindbaarin";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
 require 'dbconnect.php';
 // Aanroepen van de databaseclass als een variabele
 $database = new Database;
