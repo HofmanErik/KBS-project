7842e3f8bae54b5b78212511bf72df76047173e9
Melistick <Melistick@users.noreply.github.com>
2017-12-20 14:59:18 +0100

    reactie.php

--- Blog/admin/reacties.php
+++ Blog/admin/reacties.php
@@ -119,36 +119,9 @@ try {
   <div class="card-footer small text-muted"><?php echo "Last modified: " . date ("F d Y H:i:s.", getlastmod()); ?></div>
   </div>
 
-<<<<<<< HEAD
-=======
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Nieuw bericht</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form method="POST" action="classes/reactiebeheer.php">
-          <div class="form-group">
-            <label for="message-text" class="form-control-label">Bericht:</label>
-
-            <textarea class="form-control" id="message-text" name="bericht"></textarea>
-          </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuleren</button>
-        <button type="submit" class="btn btn-primary" name="submit">Verstuur</button>
-      </div>
->>>>>>> b9a6815157be934e9e451902ec0c47b71ad46360
       </form>
     </div>
   </div>
 </div>
-
->>>>>>> 6b405479aeb6902de3e034f9f7c17a9e53b6ab3c
 
 <?php include 'footer.php'; ?>
